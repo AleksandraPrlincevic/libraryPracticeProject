978553cf262f41617a21b0aa96480503a34404a3
Aleksandra Prlincevic Milovic <aleksandra.prlincevic@gmail.com>
2026-06-24 15:08:55 +0200

    Add method to find two most readable boks

--- Biblioteka.java
+++ Biblioteka.java
@@ -12,20 +12,23 @@ filtriranje “popularnih”*/
 import javax.swing.plaf.PanelUI;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Biblioteka {
 
-    private ArrayList<Knjiga>listaKnjiga;
+    private ArrayList<Knjiga> listaKnjiga;
     private ArrayList<Korisnik> listaKorisnika;
     private ArrayList<Pozajmica> listaPozajmica;
     private ArrayList<Pozajmica> arhivaPozajmica;
-    public Biblioteka(){
+
+    public Biblioteka() {
         this.listaKnjiga = new ArrayList<>();
         this.listaKorisnika = new ArrayList<>();
         this.listaPozajmica = new ArrayList<>();
         this.arhivaPozajmica = new ArrayList<>();
     }
-   public int ukupnoPozajmica(){
+
+    public int ukupnoPozajmica() {
         return Pozajmica.getUkupanBrPozajmica();
     }
 
@@ -34,24 +37,27 @@ public class Biblioteka {
             listaKnjiga.add(k);
         }
     }
-    public void dodajKorisnika(Korisnik... korisnici){
-        for(Korisnik c: korisnici){
+
+    public void dodajKorisnika(Korisnik... korisnici) {
+        for (Korisnik c : korisnici) {
             listaKorisnika.add(c);
         }
     }
-    public Pozajmica izdajKnjigu(Korisnik korisnik, Knjiga knjiga){
-        if(!knjiga.isDostupna()){
+
+    public Pozajmica izdajKnjigu(Korisnik korisnik, Knjiga knjiga) {
+        if (!knjiga.isDostupna()) {
             System.out.println("Žao nam je, ova knjiga je vec izdata.");
             return null;
         }
-            Pozajmica p = new Pozajmica(korisnik, knjiga);
-            knjiga.setDostupna(false);
-            p.setDanIzdavanja(LocalDate.now());
-            listaPozajmica.add(p);
-            knjiga.povecajBrPozajmica();
-            System.out.println(p);
-            return p;
+        Pozajmica p = new Pozajmica(korisnik, knjiga);
+        knjiga.setDostupna(false);
+        p.setDanIzdavanja(LocalDate.now());
+        listaPozajmica.add(p);
+        knjiga.povecajBrPozajmica();
+        System.out.println(p);
+        return p;
     }
+
     public void vratiKnjigu(Korisnik korisnik, Knjiga knjiga) {
         for (Pozajmica p : listaPozajmica) {
             if (p.getKorisnik().equals(korisnik) && p.getKnjiga().equals(knjiga)) {
@@ -64,6 +70,18 @@ public class Biblioteka {
             }
         }
     }
+
+    public ArrayList<Pozajmica> prikaziKasnjenja() {
+        ArrayList<Pozajmica> zakasnelePozajmice = new ArrayList<>();
+        for (Pozajmica p : listaPozajmica) {
+            if (p.prekoracenje()) {
+                zakasnelePozajmice.add(p);
+            }
+        }
+        System.out.println(zakasnelePozajmice);
+        return zakasnelePozajmice;
+    }
+
     public ArrayList<Knjiga> getListaKnjiga() {
         return listaKnjiga;
     }
@@ -93,7 +111,30 @@ public class Biblioteka {
     public String toString() {
         return "Biblioteka:" + listaKorisnika + listaKnjiga + listaPozajmica;
     }
-    /*public Knjiga najpopulrnija(){
 
-    }*/
+    public ArrayList<Knjiga> najpopularnije() {
+        Knjiga najcitanijaK = listaKnjiga.get(0);
+        Knjiga najcitanijaK2 = listaKnjiga.get(1);
+        if(najcitanijaK2.getBrPozajmica()<najcitanijaK.getBrPozajmica()){
+            Knjiga temp = najcitanijaK2;
+            najcitanijaK2 = najcitanijaK;
+            najcitanijaK = temp;
+        }
+        if (Pozajmica.getUkupanBrPozajmica() < 10 || listaKnjiga.size()<2) {
+            System.out.println("Nema dovoljno knjiga ili pozajmica da bi se znale najpopularnije.");
+            return null;
+        } else {
+            for (int i = 2; i < listaKnjiga.size(); i++) {
+                if (najcitanijaK.getBrPozajmica() <= listaKnjiga.get(i).getBrPozajmica()) {
+                    najcitanijaK2=najcitanijaK;
+                    najcitanijaK=listaKnjiga.get(i);
+                } else if (najcitanijaK2.getBrPozajmica() <= listaKnjiga.get(i).getBrPozajmica()){
+                    najcitanijaK2 = listaKnjiga.get(i);
+                }
+            }
+        } ArrayList<Knjiga> najcitanijeKnjige = new ArrayList<>();
+          najcitanijeKnjige.add(najcitanijaK);
+          najcitanijeKnjige.add(najcitanijaK2);
+        return najcitanijeKnjige;
+    }
 }
